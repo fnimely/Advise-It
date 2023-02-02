a7724f5e1d8025438d5fd2c073573fae2c6614a8
fnimely <dohfacsimile@gmail.com>
2023-02-01 20:09:32 -0800

    Bug Fix Corrected plan form values check

--- scripts/check.js
+++ scripts/check.js
@@ -8,6 +8,8 @@ const planFormVals = new Set();
 
 planFormValsCheck(planFormVals);
 
+const planFormValsSize = planFormVals.size;
+
 planForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -18,9 +20,10 @@ planForm.addEventListener("submit", (e) => {
     }
 
     planFormValsCheck(planFormVals);
+    console.log(planFormValsSize);
 
     // no changes made if size of set remains 4
-    if(planFormVals.size === 4){
+    if(planFormVals.size === planFormValsSize){
         pNoChange.style.display = "block";
     } else {
         pChange.style.display = "block";
@@ -30,7 +33,11 @@ planForm.addEventListener("submit", (e) => {
 
 // add for values in a set
 function planFormValsCheck(vals){
-    for (let i = 0; i < 4; i++){
-        vals.add(planForm.elements[i].value);
+    for (let i = 0; i < 5; i++){
+        if(planForm.elements[i].value.length > 0){
+            vals.add(planForm.elements[i].value);
+        }
+        // console.log(planForm.elements[i].value);
     }
+    console.log(planFormVals.size);
 }
